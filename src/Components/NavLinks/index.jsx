0ef7933fa08fde61cca57a58617d9b4edade26a5
alan.nns <alan.nns.14.11@gmail.com>
2024-03-20 10:51:28 +0700

    update: update user handle box when login

--- src/Components/NavLinks/index.jsx
+++ src/Components/NavLinks/index.jsx
@@ -8,131 +8,148 @@ import { Button } from "antd";
 import axios from "axios";
 
 const NavLinks = () => {
-  const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
-  const [isHoveredProduct, setIsHoveredProduct] = useState(false);
-  const [previousMouseOverProduct, setPreviousMouseOverProduct] =
-    useState(false);
+    const [isHoveredProduct, setIsHoveredProduct] = useState(false);
+    const [previousMouseOverProduct, setPreviousMouseOverProduct] =
+        useState(false);
 
-  const [isHoveredLogin, setIsHoveredLogin] = useState(false);
-  const [previousMouseOverLogin, setPreviousMouseOverLogin] = useState(false);
+    const [isHoveredLogin, setIsHoveredLogin] = useState(false);
+    const [previousMouseOverLogin, setPreviousMouseOverLogin] = useState(false);
 
-  const [rank, setRank] = useState({
-    data: null,
-    isLoading: false,
-  });
+    const [rank, setRank] = useState({
+        data: null,
+        isLoading: false,
+    });
 
- 
+    const [isLogin, setIsLogin] = useState(false);
+    const [user, setUser] = useState({});
 
-  const [isLogin] = useState(false);
+    useEffect(() => {
+        const userLS = sessionStorage.getItem("user");
+        if (userLS) {
+            setUser(JSON.parse(userLS));
+            setIsLogin(true);
+        }
+    }, []);
 
+    const handlerMouseEnterLogin = () => {
+        if (!isHoveredLogin) {
+            setIsHoveredLogin(true);
+            setPreviousMouseOverLogin(true);
+        }
+    };
+    const handlerMouseLeaveLogin = () => {
+        if (previousMouseOverLogin) {
+            setIsHoveredLogin(false);
+            setPreviousMouseOverLogin(false);
+        }
+    };
 
+    const handlerMouseEnterProduct = () => {
+        if (!isHoveredProduct) {
+            setIsHoveredProduct(true);
+            setPreviousMouseOverProduct(true);
+        }
+    };
 
+    return (
+        <>
+            <div className={`menu-container ${showMenu ? "hSticky" : ""}`}>
+                <div className="container">
+                    <div className="row row-hear align-items-center">
+                        <div className="col-lg-8 menu-content">
+                            <ul>
+                                <li className="menu-item">
+                                    <NavLink
+                                        to={"/"}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "menu-active"
+                                                : "menu-item"
+                                        }
+                                    >
+                                        Trang chủ
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink
+                                        to={"/about"}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "menu-active"
+                                                : "menu-item"
+                                        }
+                                    >
+                                        Hướng Dẫn
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink
+                                        to={"/products"}
+                                        onMouseMove={handlerMouseEnterProduct}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "menu-active"
+                                                : "menu-item"
+                                        }
+                                    >
+                                        Chủ đề
+                                    </NavLink>
+                                </li>
 
-  const handlerMouseEnterLogin = () => {
-    if (!isHoveredLogin) {
-      setIsHoveredLogin(true);
-      setPreviousMouseOverLogin(true);
-    }
-  };
-  const handlerMouseLeaveLogin = () => {
-    if (previousMouseOverLogin) {
-      setIsHoveredLogin(false);
-      setPreviousMouseOverLogin(false);
-    }
-  };
+                                <li className="menu-item">
+                                    <NavLink
+                                        to={"/contact"}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "menu-active"
+                                                : "menu-item"
+                                        }
+                                    >
+                                        Liên Hệ
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-2 menu-control">
+                            <div className="icon">
+                                <NavLink
+                                    className="icon-login"
+                                    onMouseMove={handlerMouseEnterLogin}
+                                >
+                                    <FaRegUser style={{ fontSize: "25px" }} />
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-  const handlerMouseEnterProduct = () => {
-    if (!isHoveredProduct) {
-      setIsHoveredProduct(true);
-      setPreviousMouseOverProduct(true);
-    }
-  };
-
-  return (
-    <>
-      <div className={`menu-container ${showMenu ? "hSticky" : ""}`}>
-        <div className="container">
-          <div className="row row-hear align-items-center">
-            <div className="col-lg-8 menu-content">
-              <ul>
-                <li className="menu-item">
-                  <NavLink
-                    to={"/"}
-                    className={({ isActive }) =>
-                      isActive ? "menu-active" : "menu-item"
-                    }
-                  >
-                    Trang chủ
-                  </NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink
-                    to={"/about"}
-                    className={({ isActive }) =>
-                      isActive ? "menu-active" : "menu-item"
-                    }
-                  >
-                    Hướng Dẫn
-                  </NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink
-                    to={"/products"}
-                    onMouseMove={handlerMouseEnterProduct}
-                    className={({ isActive }) =>
-                      isActive ? "menu-active" : "menu-item"
-                    }
-                  >
-                    Chủ đề
-                  </NavLink>
-                </li>
-               
-                <li className="menu-item">
-                  <NavLink
-                    to={"/contact"}
-                    className={({ isActive }) =>
-                      isActive ? "menu-active" : "menu-item"
-                    }
-                  >
-                    Liên Hệ
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div className="col-lg-2 menu-control">
-              
-              <div className="icon">
-                <NavLink
-                  className="icon-login"
-                  onMouseMove={handlerMouseEnterLogin}
+                <div
+                    className={`form-login ${isHoveredLogin ? "" : "d-none"}`}
+                    onMouseLeave={handlerMouseLeaveLogin}
                 >
-                  <FaRegUser style={{ fontSize: "25px" }} />
-                </NavLink>
-              </div>
-              </div>
-          </div>
-        </div>
-        
-        <div
-          className={`form-login ${isHoveredLogin ? "" : "d-none"}`}
-          onMouseLeave={handlerMouseLeaveLogin}
-        >
-          <div className="link-login">
-            <NavLink to={`${isLogin ? "/accont" : "/login"}`}>{`${
-              isLogin ? "Tài khoản" : "Đăng nhập"
-            }`}</NavLink>
-          </div>
-          <div className = "link-register">
-            <NavLink className={`${isLogin ? "d-none":""}` } to={"/register"} >
-              Đăng Ký
-            </NavLink>
-            <Button className={`${isLogin ? "link-register":"d-none"}`} onClick={() => deleteSecction()}>Đăng xuất</Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                    <div className="link-login">
+                        <NavLink to={`${isLogin ? "/accont" : "/login"}`}>{`${
+                            isLogin ? "Tài khoản" : "Đăng nhập"
+                        }`}</NavLink>
+                    </div>
+                    <div className="link-register">
+                        {isLogin ? (
+                            <Button
+                                className={`link-register`}
+                                onClick={() => deleteSecction()}
+                            >
+                                Đăng xuất
+                            </Button>
+                        ) : (
+                            <NavLink to={"/register"}>Đăng Ký</NavLink>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default NavLinks;
